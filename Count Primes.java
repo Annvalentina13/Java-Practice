02ed4ca0b672d523c7428cb8b2c21f2a75ecfe70
Annie Valentina A <Annievalentina1307@gmail.com>
2026-01-05 22:15:40 +0530

    Create Count Primes.java

---
+++ Count Primes.java
@@ -0,0 +1,54 @@
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 5 * 106
+*/
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+        
+        // Step 1: assume all are prime
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Step 2: eliminate multiples
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Step 3: count primes
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+
+        return count;
+    }
+}
+
