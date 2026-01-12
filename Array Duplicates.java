c975a9f1486c338aaf44fd813e2939b4e72d7368
Annie Valentina A <Annievalentina1307@gmail.com>
2026-01-12 18:14:03 +0530

    Add findDuplicates method to identify duplicates
    Implement a method to find duplicates in an array.

---
+++ Array Duplicates.java
@@ -0,0 +1,38 @@
+/*
+Given an array arr[] of size n, containing elements from the range 1 to n, and each element appears at most twice, return an array of all the integers that appears twice.
+
+Note: You can return the elements in any order but the driver code will print them in sorted order.
+
+Examples:
+
+Input: arr[] = [2, 3, 1, 2, 3]
+Output: [2, 3] 
+Explanation: 2 and 3 occur more than once in the given array.
+Input: arr[] = [3, 1, 2] 
+Output: []
+Explanation: There is no repeating element in the array, so the output is empty.
+Constraints:
+1 ≤ n ≤ 106
+1 ≤ arr[i] ≤ n
+
+*/
+
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int index = Math.abs(arr[i]) - 1;
+
+            if (arr[index] < 0) {
+                result.add(index + 1);
+            } else {
+                arr[index] = -arr[index];
+            }
+        }
+
+        return result;
+    }
+}
