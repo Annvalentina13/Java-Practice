f7a1eeb175c47c0bc893c055a5f0099bbe86dd13
Annie Valentina A <Annievalentina1307@gmail.com>
2026-02-04 21:48:50 +0530

    Add Even Odd Game logic in Even Odd Game.java
    Implement Even Odd Game logic to determine the winner based on the number of apples.

---
+++ Even Odd Game.java
@@ -0,0 +1,25 @@
+/*
+Given a number, represents number of apples in a bag. You and your friend are playing a game. Both of you are picking one apple turn wise from the bag. It is given that first attempt is always by you. The person picking the last apple will be the winner. Find out the winner if the quantity of apples is given to you as a number.
+If you will win: return "You" (without quotes)
+If your friend will win: return "Friend" (without quotes)
+
+Examples:
+
+Input: n = 9
+Output: You
+Input: n = 4
+Output: Friend
+Constraints:
+1 <= n <= 100
+
+*/
+
+class Solution {
+    public static String utility(int n) {
+        if (n % 2 == 0) {
+            return "Friend";
+        } else {
+            return "You";
+        }
+    }
+}
